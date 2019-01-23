1fe06cd4072c604abb79b3e702c50714dccca670
Jackson Zhang <zbcjackson@gmail.com>
2019-01-23 12:12:32 +0800

    Cache mvn install step; Change port to support integration test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM maven:slim as build-stage
-COPY ./ ./
+COPY ./pom.xml ./
 RUN mvn install
+COPY ./ ./
 CMD mvn spring-boot:run
 
 
--- src/main/java/Query.java
+++ src/main/java/Query.java
@@ -4,7 +4,7 @@ import org.springframework.web.client.RestTemplate;
 public class Query implements GraphQLQueryResolver {
     public Quote getQuoteOfTheDay(){
         RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://backend:8090/quotes/today", Quote.class);
+        Quote quote = restTemplate.getForObject("http://backend:8080/quotes/today", Quote.class);
         return quote;
     }
 }
