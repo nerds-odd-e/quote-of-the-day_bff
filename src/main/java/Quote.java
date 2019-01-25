196f06ab9bfa7056ff0347c9c0f995b598c02070
Ivan <vanzaj@gmail.com>
2019-01-25 16:59:38 +0800

    use same schema

--- src/main/java/Quote.java
+++ src/main/java/Quote.java
@@ -2,40 +2,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
-    private long id;
-    private String content;
-    private String originalAuthor;
-    private String submitter;
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setOriginalAuthor(String originalAuthor) {
-        this.originalAuthor = originalAuthor;
-    }
-
-    public void setSubmitter(String submitter) {
-        this.submitter = submitter;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getOriginalAuthor() {
-        return originalAuthor;
-    }
-
-    public String getSubmitter() {
-        return submitter;
-    }
-
-    public long getId() {
-        return id;
-    }
+    public String id;
+    public String Quote;
+    public String Author;
+    public String Contributor;
+    public String Location;
+    public String Date;
+    public String Language;
+    public String ContributionDate;
 }
